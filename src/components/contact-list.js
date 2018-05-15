29ae660a7de7059bbe5aadf2d3844106815b1207
Efren Garcia Home <efren.gl@gmail.com>
2018-05-15 07:09:10 -0500

    first commit

---
+++ src/components/contact-list.js
@@ -0,0 +1,22 @@
+// src/components/contact-list
+
+import React from 'react';
+
+export default function ContactList({contacts}){
+
+  const list = () => {
+    return contacts.map(contact => {
+      return (
+        <li key={contact._id}>{contact.name.first} {contact.name.last}</li>
+      )
+    })
+  }
+
+  return (
+    <div>
+      <ul>
+        { list() }
+      </ul>
+    </div>
+  )
+}
